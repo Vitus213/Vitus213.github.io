1fdea2d33aaab9a630235a974b19e55c513fd4cd
Samuka007 <dai-samuel@outlook.com>
2025-12-27 20:50:47 +0800

    update ref

--- flake.nix
+++ flake.nix
@@ -2,22 +2,29 @@
   description = "Node.js development environment with pnpm";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
-      in 
+      in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs
-            pnpm_9
+            # pnpm_9
+            pnpm_10
           ];
         };
       }
